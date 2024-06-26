81999bd9c349915ba93d28c55f5e799708323388
Larry Reaux <larryreaux@gmail.com>
2024-06-26 14:55:18 -0500

    worked on formatting and main page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,13 @@
-import { Image, StyleSheet, Platform, Text, TextInput } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  Text,
+  TextInput,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
@@ -8,37 +17,99 @@ import { View } from "react-native";
 import { useGlobalContext } from "@/context/GlobalProvider";
 import { IngredientListItem } from "@/helpers/Ingredients";
 import { DropdownComponent as Dropdown } from "@/components/Dropdown";
+import FormField from "@/components/FormField";
+import { SafeAreaView } from "react-native";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 export default function HomeScreen() {
+  const backgroundColor = useThemeColor({}, "background");
+  const textColor = useThemeColor({}, "text");
+  console.log(`bg-[${backgroundColor.replace(/\s+/g, "_")}]`);
   const { ingredients, recipeData } = useGlobalContext();
   const ingredientsDropdown = ingredients.map((ing: IngredientListItem) => ({
     label: ing.name,
     value: ing.name,
   }));
-  const map = new Array(4).fill(0);
+
   return (
-    <View className="justify-center w-screen h-screen ">
-      {map.map((item, i) => (
-        <View key={i}>
-          <Dropdown data={ingredientsDropdown} index={i} />
-          <TextInput
-            value={recipeData.ingredients[i].details[0].toString()}
-            keyboardType="numeric"
-            placeholder="0"
-          />
-          <TextInput
-            value={recipeData.ingredients[i].brix.toString()}
-            keyboardType="numeric"
-            placeholder="0"
-          />
-          <TextInput
-            value={recipeData.ingredients[i].details[1].toString()}
-            keyboardType="numeric"
-            placeholder="0"
-          />
-        </View>
-      ))}
-    </View>
+    <SafeAreaView
+      className={"h-screen items-center justify-center"}
+      style={{
+        backgroundColor,
+      }}
+    >
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View>
+            <Text className="text-4xl text-center" style={{ color: textColor }}>
+              Recipe Builder
+            </Text>
+            <Text
+              style={{ color: textColor }}
+              className="text-2xl py-4 text-center"
+            >
+              Primary Ingredients
+            </Text>
+            {recipeData.ingredients.map(
+              (item: IngredientListItem, i: number) => (
+                <ThemedView key={i}>
+                  <Dropdown data={ingredientsDropdown} index={i} />
+
+                  <View className="flex-row items-center w-screen h-16 justify-between">
+                    <View className="mx-4 text-center items-center">
+                      <ThemedText
+                        type="subtitle"
+                        className="font-semibold my-2"
+                      >
+                        Brix
+                      </ThemedText>
+
+                      <TextInput
+                        className="flex-1 text-center text-base bg-white border-2 w-24 bg-black-100 rounded-2xl focus:border-secondary"
+                        value={recipeData.ingredients[i].brix.toString()}
+                        keyboardType="numeric"
+                        placeholder="0"
+                      />
+                    </View>
+                    <View className="mx-4 text-center items-center">
+                      <ThemedText
+                        type="subtitle"
+                        className="font-semibold my-2"
+                      >
+                        Weight
+                      </ThemedText>
+
+                      <TextInput
+                        className="flex-1 text-center text-base bg-white border-2 w-24 bg-black-100 rounded-2xl focus:border-secondary"
+                        value={recipeData.ingredients[i].details[0].toString()}
+                        keyboardType="numeric"
+                        placeholder="0"
+                      />
+                    </View>
+
+                    <View className="mx-4 text-center items-center">
+                      <ThemedText
+                        type="subtitle"
+                        className="font-semibold my-2"
+                      >
+                        Volume
+                      </ThemedText>
+
+                      <TextInput
+                        className="flex-1 text-center text-base bg-white border-2 w-24 bg-black-100 rounded-2xl focus:border-secondary"
+                        value={recipeData.ingredients[i].details[1].toString()}
+                        keyboardType="numeric"
+                        placeholder="0"
+                      />
+                    </View>
+                  </View>
+                </ThemedView>
+              )
+            )}
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
